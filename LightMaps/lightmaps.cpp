66ac916f7d8003974ecbb836ed4d5ad3cdde745a
Mikhail Firulin <mfirulin@gmail.com>
2017-09-14 12:42:13 +0300

    Update lightmaps.cpp

--- LightMaps/lightmaps.cpp
+++ LightMaps/lightmaps.cpp
@@ -27,21 +27,34 @@ LightMaps::LightMaps(QWidget *parent) :
     connect(zoomSlider, &QSlider::valueChanged, this, &LightMaps::zoomMap);
 
     coordinateLabel = new QLabel;
-    QPalette palette;
-    palette.setColor(QPalette::WindowText, QColor(Qt::darkGray).darker(150));
-    coordinateLabel->setPalette(palette);
+//     QPalette palette;
+//     palette.setColor(QPalette::WindowText, QColor(Qt::darkGray).darker(150));
+//     coordinateLabel->setPalette(palette);
     QVBoxLayout *vLayout = new QVBoxLayout;
     vLayout->addStretch();
     vLayout->addWidget(coordinateLabel);
-
+        
+    scaleTextLabel = new QLabel;
+    scaleImageLabel = new QLabel;
     QHBoxLayout *hLayout = new QHBoxLayout;
-    hLayout->setMargin(20);
-    hLayout->addLayout(vLayout);
-    hLayout->addStretch();
-    hLayout->addWidget(zoomSlider);
-    this->setLayout(hLayout);
+    hLayout->addWidget(scaleTextLabel);
+    hLayout->addWidget(scaleImageLabel);
+
+    QVBoxLayout *vLayout2 = new QVBoxLayout;
+    vLayout2->addWidget(zoomSlider);
+    vLayout2->setAlignment(zoomSlider, Qt::AlignRight);
+    vLayout2->addLayout(hLayout);    
+        
+    QHBoxLayout *hLayout2 = new QHBoxLayout;
+    hLayout2->setMargin(10);
+    hLayout2->addLayout(vLayout);
+    hLayout2->addStretch();
+    hLayout2->addLayout(vLayout2);
+
+    this->setLayout(hLayout2);
 
     setMouseTracking(true);
+    displayScale();
 }
 
 void LightMaps::setCenter(qreal lat, qreal lng)
@@ -68,6 +81,7 @@ void LightMaps::zoomMap()
     m_map->invalidate();
     update();
     this->setFocus();
+    displayScale();
 }
 
 void LightMaps::resizeEvent(QResizeEvent *)
@@ -148,7 +162,6 @@ void LightMaps::wheelEvent(QWheelEvent *event)
 
     zoomSlider->setValue(m_map->zoom);
     m_map->invalidate();
-    update();
     this->setFocus();
 }
 
@@ -177,4 +190,62 @@ void LightMaps::keyPressEvent(QKeyEvent *event)
     if (event->key() == Qt::Key_Z || event->key() == Qt::Key_Select) {
         dragPos = QPoint(width() / 2, height() / 2);
     }
+}
+
+void LightMaps::displayScale()
+{
+    qreal zn = static_cast<qreal>(1 << m_map->zoom);
+    qreal latitude = m_map->latitude / 100;
+    qreal resolution = 156543.03/*meters/pixel on equator*/ * cos(latitude) / zn;
+//    qreal scl = physicalDpiX() * 39.37/*inches/meter*/ * resolution;
+
+    qreal dist;
+    switch(m_map->zoom) {
+    case 19: dist = 10; break;
+    case 18: dist = 20; break;
+    case 17: dist = 40; break;
+    case 16: dist = 80; break;
+    case 15: dist = 150; break;
+    case 14: dist = 350; break;
+    case 13: dist = 700; break;
+    case 12: dist = 1500; break;
+    case 11: dist = 2500; break;
+    case 10: dist = 5000; break;
+    case 9: dist = 10000; break;
+    case 8: dist = 20000; break;
+    case 7: dist = 40000; break;
+    case 6: dist = 100000; break;
+    case 5: dist = 150000; break;
+    case 4: dist = 350000; break;
+    case 3: dist = 700000; break;
+    case 2: dist = 1500000; break;
+    case 1: dist = 2500000; break;
+    case 0: dist = 5000000; break;
+    default: dist = 0;
+    }
+
+    int scale = qRound(dist / resolution);
+
+    QPicture pic;
+    QPainter painter(&pic);
+    //QPen pen(QColor(65, 168, 234)); // Blue color similar to the slider.
+    QPen pen(Qt::black);
+    pen.setWidth(2);
+    painter.setPen(pen);
+    painter.drawLine(0, 2, scale, 2); // draw: ------
+    painter.drawLine(0, 0, 0, 4); // draw: |-
+    painter.drawLine(scale, 0, scale, 4); // draw: -|
+    painter.end();
+
+    QString suffix;
+    if(dist > 1000) {
+        dist /= 1000;
+        suffix = tr("km");
+    }
+    else
+        suffix = tr("m");
+
+    QString s = QString::number(dist) + " " + suffix;
+    scaleTextLabel->setText(s);
+    scaleImageLabel->setPicture(pic);
 }
